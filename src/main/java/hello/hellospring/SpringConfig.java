be16afea81d3c58b69514e0bf2d72ff5f25c7ec9
김멍몽 <wjdahrrla@gmail.com>
2023-12-23 17:42:14 +0900

    202312231742

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,27 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Bean // 빈을 직접 등록한다.
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
